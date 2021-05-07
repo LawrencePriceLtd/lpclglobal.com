f7834b1d4741c4d244b2ef51a9922a70367accce
wiztemple <wiztemple7@gmail.com>
2021-05-07 08:38:13 +0100

    add link to blog

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -76,14 +76,14 @@ const Home = () => {
               alt="mechanism"
             />
             <div className="bg-altgrey relative h-full w-full">
-              <button type="button" className="dark:bg-orange w-full absolute bottom-2 flex items-center space-x-6 text-lg font-medium md:pl-20 pl-10 pr-10 text-coregray  dark:text-white pb-6 md:pt-36 pt-6 flex-grow-0">
+              <Link to="/blog" type="button" className="dark:bg-orange w-full absolute bottom-2 flex items-center space-x-6 text-lg font-medium md:pl-20 pl-10 pr-10 text-coregray  dark:text-white pb-6 md:pt-36 pt-6 flex-grow-0">
                 <span>Visit Blog</span>
                 <span>
                   <svg width="27" height="16" viewBox="0 0 27 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM26.7071 8.70711C27.0976 8.31658 27.0976 7.68342 26.7071 7.29289L20.3431 0.928932C19.9526 0.538408 19.3195 0.538408 18.9289 0.928932C18.5384 1.31946 18.5384 1.95262 18.9289 2.34315L24.5858 8L18.9289 13.6569C18.5384 14.0474 18.5384 14.6805 18.9289 15.0711C19.3195 15.4616 19.9526 15.4616 20.3431 15.0711L26.7071 8.70711ZM1 9H26V7H1V9Z" fill={theme === 'light' ? '#545454' : '#fff'} />
                   </svg>
                 </span>
-              </button>
+              </Link>
             </div>
           </div>
         </div>
